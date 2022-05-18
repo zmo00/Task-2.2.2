585354145943874180ad42ccbdbfbed507de0821
zmo00 <zmo00@yandex.ru>
2022-05-18 16:35:23 +0300

    Item2

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,4 +6,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/cars")
 public class CarsController {
+
 }
---
+++ src/main/java/web/models/Car.java
@@ -0,0 +1,7 @@
+package web.models;
+
+public class Car {
+    private String mark;
+    private String series;
+    private int number;
+}
